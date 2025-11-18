8d5f116c47e26ae3a87c228917b71580700522ae
kaiolincoln <kaiolincoln2001@hotmail.com>
2025-11-18 15:56:40 -0300

    feat: ajustes finais

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "DevMotors - Sua oficina de confiança",
   description: "Oficina de carros especializada em manutenção e reparos.",
+  keywords: ["oficina", "oficina carros", "carros",  "manutencao de carros"],
+  openGraph: {
+    title: "DevMotors - Sua oficina de confiança",
+    images: [ `${process.env.NEXT_PUBLIC_URL}/logo.jpg`]
+  },
+  robots:{
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    }
+  }
 };
 
 export default function RootLayout({
--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,10 +1,14 @@
-export async function getDataHome(){
-    try{
+import { redirect, notFound } from "next/navigation"; 
+
+export async function getDataHome() {
+    try {
         const url = `${process.env.NEXT_PUBLIC_COSMIC_BUCKET}/objects/69148677e7349beda29198ed?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,type`;
 
-        const res = await fetch(url);
+        const res = await fetch(url, { 
+            next: { revalidate: 120 } 
+        });
 
-        if(!res.ok){
+        if (!res.ok) {
             throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
         }
 
@@ -14,19 +18,21 @@ export async function getDataHome(){
 
         return data;
 
-    }catch(err){
+    } catch (err) {
         console.error("Erro em getDataHome:", err);
         throw new Error('Failed to fetch data');
     }
 }
 
-export async function getSubMenu(){
-    try{
+export async function getSubMenu() {
+    try {
         const url = `${process.env.NEXT_PUBLIC_COSMIC_BUCKET}/objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&read_key=${process.env.READ_KEY}&depth=1&props=slug,title`;
         
-        const res = await fetch(url);
+        const res = await fetch(url, { 
+            next: { revalidate: 120 }
+        });
 
-        if(!res.ok){
+        if (!res.ok) {
             throw new Error(`Failed to fetch menu data: ${res.status} ${res.statusText}`);
         }
 
@@ -36,9 +42,44 @@ export async function getSubMenu(){
 
         return data;
 
-    }catch(err){
+    } catch (err) {
         console.error("Erro em getSubMenu:", err);
         throw new Error("Failed to fetch menu data");
     }
 }
 
+export async function getItemBySlug(itemSlug: string) {
+    const baseUrl = `${process.env.NEXT_PUBLIC_COSMIC_BUCKET}/objects`;
+
+    const queryParams = new URLSearchParams({
+        query: JSON.stringify({
+            slug: itemSlug
+        }),
+        props: 'slug,title,content,metadata,type', 
+        read_key: process.env.READ_KEY as string
+    });
+
+    const url = `${baseUrl}?${queryParams.toString()}`;
+
+    try {
+        const res = await fetch(url, { 
+            next: { revalidate: 120 } 
+        });
+
+        if (!res.ok) {
+            throw new Error(`Failed to get item by slug: ${res.status}`);
+        }
+
+        const data = await res.json();
+        
+        if (!data.objects || data.objects.length === 0) {
+            throw new Error("Item não encontrado");
+        }
+
+        return data;
+
+    } catch (err) {
+        console.error("Erro em getItemBySlug:", err);
+        throw err; 
+    }
+}
